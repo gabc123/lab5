45a8bf542eb375ef2b565a8fe15f34bc39aad15a
mats <mats@bajs.bajs>
2015-10-17 15:17:58 +0200

    omg

--- src/UIGraphics/LoginScreen.java
+++ src/UIGraphics/LoginScreen.java
@@ -5,17 +5,70 @@
  */
 package UIGraphics;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.GraphicsContext;
+
 /**
  *
  * @author mats
  */
-public class LoginScreen {
+public class LoginScreen extends Application{
     
-    public void play(){}
+    private Group root;
+    private Image image = new Image("/Vatten.png", false);
+    private Image imagebut = new Image("/Playknapp.png", false);
+    Button but;
+    
+    @Override
+    public void start(Stage stage) throws Exception{
+        
+	but = new Button();
+	but.setGraphic(new ImageView(imagebut));
+
+        //but.setOnAction(new EventHandler<ActionEvent>());
+	
+	root = new Group();
+	Scene scene = new Scene(root, 1024, 720, Color.GREEN);
+	Canvas canvas = new Canvas(scene.getWidth(), scene.getHeight());
+
+	but.setLayoutX(canvas.getWidth()/2 - (imagebut.getWidth()/2));
+        but.setLayoutY(canvas.getHeight()/2 - (imagebut.getHeight()/2));
+	
+	
+	GraphicsContext gc = canvas.getGraphicsContext2D();
+	gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+	gc.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight());
+	
+	root.getChildren().add(canvas);
+	root.getChildren().add(but);
+	
+	
+	stage.setTitle("Start");
+	stage.setScene(scene);
+	stage.setResizable(false);
+	stage.sizeToScene();
+	stage.show();
+        
+    }
+    
+    public void play(){
+    
+    }
     
     public void loadgame(){}
     
     public void showhighscore(){}
 
     public void keybindings(){}
+    
 }
