9aa9ab0e6964ef707c9676055ada2df35d28a7b7
mats <mats@bajs.bajs>
2015-10-22 02:51:55 +0200

    keybindings

--- src/UIGraphics/Lobby.java
+++ src/UIGraphics/Lobby.java
@@ -5,6 +5,7 @@
  */
 package UIGraphics;
 
+
 import Controller.KeyboardController;
 import GameObjects.Player.Playerinfo;
 import java.util.ArrayList;
@@ -18,7 +19,10 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import lab5game.GameSetup;
@@ -35,9 +39,27 @@ public class Lobby {
     private CheckBox aiCheck;
     private GameSetup gameSetup;
     private Button start;
+    private Button jumpbuttonp1;
+    private Button leftbuttonp1;
+    private Button rightbuttonp1;
+    private Button jumpbuttonp2;
+    private Button leftbuttonp2;
+    private Button rightbuttonp2;
     private String player1name;
     private String player2name;
     private KeyboardController player1input;
+    private KeyboardController player2input;
+    private boolean enterNewKey;
+    private int player;
+    private int whatkey;
+    
+    private KeyCode jumpbutp1;
+    private KeyCode leftbutp1;
+    private KeyCode rightbutp1;
+    
+    private KeyCode jumpbutp2;
+    private KeyCode leftbutp2;
+    private KeyCode rightbutp2;
     
     
     public Lobby(Stage stage, GameSetup gameSetup_){
@@ -53,36 +75,131 @@ public class Lobby {
     
     public void lobbysetup(){
         Stage stage = this.Lobbystage;
+        enterNewKey = false;
+        player = 0;
+        whatkey = 0;
+        jumpbutp1 = KeyCode.SPACE;;
+        leftbutp1 = KeyCode.A;
+        rightbutp1 = KeyCode.D;
+        jumpbutp2 = KeyCode.SPACE;
+        leftbutp2 = KeyCode.LEFT;
+        rightbutp2 = KeyCode.RIGHT;
+        
+        
+        EventHandler handler;
+        handler = new EventHandler<KeyEvent>() {
+        @Override
+            public void handle(KeyEvent event) {
+                if(enterNewKey) {
+                    setKey(event.getCode());
+                    enterNewKey = false;
+                    event.consume();
+                }
+                else{event.consume();}
+            }
+        };
+        
+        Label jumpp1 = new Label("Jump");
+        jumpbuttonp1 = new Button();
+            jumpbuttonp1.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event){
+                    player = 1;
+                    enterNewKey = true;
+                    whatkey = 1;
+            }   
+        });
+        HBox jumpcon = new HBox(10);
+        
+        Label leftp1 = new Label("Left");
+        leftbuttonp1 = new Button();
+            leftbuttonp1.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event){
+                    player = 1;
+                    enterNewKey = true;
+                    whatkey = 2;
+            }   
+        });
+        HBox leftcon = new HBox(10);
+        
+        Label rightp1 = new Label("Right");
+        rightbuttonp1 = new Button();
+            rightbuttonp1.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event){
+                    player = 1;
+                    enterNewKey = true;
+                    whatkey = 3;
+            }   
+        });
+        HBox rightcon = new HBox(10);
+        
+        Label jumpp2 = new Label("Jump");
+        jumpbuttonp2 = new Button();
+            jumpbuttonp2.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event){
+                    player = 2;
+                    enterNewKey = true;
+                    whatkey = 1;
+            }   
+        });
+        HBox jumpcon2 = new HBox(10);
+        
+        Label leftp2 = new Label("Left");
+        leftbuttonp2 = new Button();
+            leftbuttonp2.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event){
+                    player = 2;
+                    enterNewKey = true;
+                    whatkey = 2;
+            }   
+        });
+        HBox leftcon2 = new HBox(10);
+        
+        Label rightp2 = new Label("Right");
+        rightbuttonp2 = new Button();
+            rightbuttonp2.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event){
+                    player = 2;
+                    enterNewKey = true;
+                    whatkey = 3;
+            }   
+        });
+        HBox rightcon2 = new HBox(10);
+            
+            
 
         Label player1 = new Label("Player 1 Name:");
         TextField player1Name = new TextField ();
-        HBox namecon = new HBox();
+        HBox namecon = new HBox(10);
         
         Label player2 = new Label("Player 2 Name:");
         TextField player2Name = new TextField ();
-        HBox namecon2 = new HBox();
+        HBox namecon2 = new HBox(10);
         
         aiCheck = new CheckBox();
         start = new Button("Start");
         
         start.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent event) {
                 player1name = player1Name.getText();
                 playerInfo.get(0).setPlayerName(player1name);
                 
-                player1input = new KeyboardController(KeyCode.A, KeyCode.D, KeyCode.getKeyCode(player1name.toUpperCase()));
+                player1input = new KeyboardController(leftbutp1, rightbutp1, jumpbutp1);
                 playerInfo.get(0).setKeyboard(player1input);
                 
                 player2name = player2Name.getText();
                 playerInfo.get(1).setPlayerName(player2name);
+                
+                player2input = new KeyboardController(leftbutp2, rightbutp2, jumpbutp2);
+                playerInfo.get(1).setKeyboard(player2input);
+                        
                 //System.out.println(player1name);
                 //playerInfo.add(new Playerinfo(player1name,input));
                 if(aiCheck.isSelected() == true){
                     playerInfo.remove(1);
                     numOfAi++;
                 }
+                stage.removeEventHandler(KeyEvent.ANY, handler);
                 startMap();
             }
         });
@@ -97,16 +214,59 @@ public class Lobby {
         
         aiCheck.setText("Ai");
         
-        namecon.setSpacing(10);
         namecon.getChildren().addAll(player1, player1Name);
         
-        namecon2.setSpacing(10);
         namecon2.getChildren().addAll(player2, player2Name, aiCheck);
-
-        HBox namecon4 = new HBox();
-        namecon4.setSpacing(100);
-        namecon4.getChildren().addAll(namecon, namecon2);
         
+        jumpcon.getChildren().addAll(jumpp1, jumpbuttonp1);
+        
+        leftcon.getChildren().addAll(leftp1, leftbuttonp1);
+        
+        rightcon.getChildren().addAll(rightp1, rightbuttonp1);
+                
+        jumpcon2.getChildren().addAll(jumpp2, jumpbuttonp2);
+        
+        leftcon2.getChildren().addAll(leftp2, leftbuttonp2);
+        
+        rightcon2.getChildren().addAll(rightp2, rightbuttonp2);
+        
+        Label wichplayer = new Label("Player1:");
+        VBox keyconp1 = new VBox(10);
+        keyconp1.getChildren().addAll(wichplayer,jumpcon, leftcon, rightcon);
+        
+        Label wichplayer2 = new Label("Player2:");
+        VBox keyconp2 = new VBox(10);
+        keyconp1.getChildren().addAll(wichplayer2,jumpcon2, leftcon2, rightcon2);
+        
+        VBox keyconinfo = new VBox(10);
+        Label info = new Label("Choose buttons to controll your caracter with:");
+        keyconinfo.getChildren().addAll(info, keyconp1, keyconp2);
+        
+        /**HBox namecon4 = new HBox();
+        namecon4.setSpacing(10);
+        namecon4.getChildren().addAll(namecon, namecon2, keyconinfo);
+        namecon4.setPrefWidth(scene.getWidth());*/
+        
+        FlowPane namecon4 = new FlowPane();
+        namecon4.setVgap(10);
+        namecon4.setHgap(10);
+        namecon4.getChildren().addAll(namecon, namecon2, keyconinfo);
+        namecon4.setPrefWidth(scene.getWidth());
+        
+        
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, handler);
+                
+        /**stage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+        @Override
+            public void handle(KeyEvent event) {
+                if(enterNewKey) {
+                    setKey(event.getCode());
+                    enterNewKey = false;
+                }
+            }
+        });*/
+        
+
         root.getChildren().add(canvas);
         root.getChildren().add(namecon4);
         root.getChildren().add(start);
@@ -132,6 +292,26 @@ public class Lobby {
         
         return playerInfo;
     }
+    
+    private void setKey(KeyCode key){
+        
+        if(player == 1){
+            switch (whatkey){
+            case 1: jumpbutp1 = key; break;
+            case 2: leftbutp1 = key; break;
+            case 3: rightbutp1 = key; break;
+            default: break;
+            }
+        }
+        if(player == 2){
+            switch (whatkey){
+            case 1: jumpbutp2 = key; break;
+            case 2: leftbutp2 = key; break;
+            case 3: rightbutp2 = key; break;
+                default: break;
+            }
+        }
+    
+    }
+
 }
-
-
