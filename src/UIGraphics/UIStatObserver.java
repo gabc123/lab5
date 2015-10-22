fe2a4981dcdcc01dee5440a24e22d946b0b7ea51
gabc123 <somtingsomting@gmail.com>
2015-10-22 16:51:47 +0200

    show player stats

--- src/UIGraphics/UIStatObserver.java
+++ src/UIGraphics/UIStatObserver.java
@@ -20,44 +20,52 @@ import javafx.scene.paint.Color;
  *
  * @author mats
  */
-public class UIStatObserver implements Observer{
-        
-        private StackPane root;
-        private ArrayList<Player> players;
-    
-        public UIStatObserver(StackPane root_){
-            this.root = root_;
-        }
-        
-        public void createUI(){
-        
-            Label player1name = new Label(players.get(0).getName());
-            Label player2name = new Label(players.get(1).getName());
-            player1name.setTextFill(Color.RED);
-            player2name.setTextFill(Color.RED);
-            Label player1health = new Label(players.get(0).getName());
-            Label player2health = new Label(players.get(1).getName());
-            player1health.setTextFill(Color.RED);
-            player2health.setTextFill(Color.RED);
-            VBox playerinfocon1 = new VBox(5);
-            playerinfocon1.getChildren().addAll(player1name, player1health);
-            VBox playerinfocon2 = new VBox(5);
-            playerinfocon2.getChildren().addAll(player2name, player2health);
-            HBox namecon = new HBox(880);
-            namecon.setAlignment(Pos.BOTTOM_CENTER);
-            namecon.getChildren().addAll(playerinfocon1, playerinfocon2);
-            root.getChildren().addAll(namecon);
-    
-        }
-    
-    public void setPlayers(ArrayList<Player> players_){
-        this.players = players_;
-    
+public class UIStatObserver implements Observer {
+
+    //private StackPane root;
+    private Label player1name;
+    private Label player2name;
+    private Label player1health;
+    private Label player2health;
+    private ArrayList<Player> players;
+
+    public UIStatObserver(StackPane root, ArrayList<Player> players) {
+        super();
+        this.players = players;
+        createUI(root);
+
     }
+
+    private void createUI(StackPane root) {
+        player1name = new Label(players.get(0).getName());
+        player2name = new Label(players.get(1).getName());
+        player1name.setTextFill(Color.RED);
+        player2name.setTextFill(Color.RED);
+        player1health = new Label("Health:" + players.get(0).currentHealth());
+        player2health = new Label("Health:" + players.get(1).currentHealth());
+        player1health.setTextFill(Color.RED);
+        player2health.setTextFill(Color.RED);
+        VBox playerinfocon1 = new VBox(5);
+        playerinfocon1.getChildren().addAll(player1name, player1health);
+        VBox playerinfocon2 = new VBox(5);
+        playerinfocon2.getChildren().addAll(player2name, player2health);
+        HBox namecon = new HBox(880);
+        namecon.setAlignment(Pos.BOTTOM_CENTER);
+        namecon.getChildren().addAll(playerinfocon1, playerinfocon2);
+
+        root.getChildren().addAll(namecon);
+    }
+
+    public void setPlayers(ArrayList<Player> players_) {
+        this.players = players_;
+
+    }
+
     @Override
     public void update(Observable o, Object arg) {
         
-    
+        this.player1health.setText("Health:" + players.get(0).currentHealth());
+        this.player2health.setText("Health:" + players.get(1).currentHealth());
     }
-    
+
 }
